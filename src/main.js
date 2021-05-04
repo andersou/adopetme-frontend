ac992792457e31ead8c5d66d9f89e9475baea0b8
Anderson Souza <andersom.eugenio@gmail.com>
2021-05-03 23:21:52 -0300

    organizei a ordem

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Buefy from "buefy";
+import router from "./router";
+import store from "./store";
 
 // import "buefy/dist/buefy.css";
 import "./styles/style.scss";
-
-import router from './router'
-import store from './store'
 
 Vue.use(Buefy);
 Vue.config.productionTip = false;
@@ -14,5 +13,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
