9b7a2ced02a0310e288f419105e8f555cb9d1543
Anderson Souza <andersom.eugenio@gmail.com>
2021-05-03 22:48:58 -0300

    adicionado vuex

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import Buefy from "buefy";
 import "./styles/style.scss";
 
 import router from './router'
+import store from './store'
 
 Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App)
 }).$mount("#app");
