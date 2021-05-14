8d368a99a5af1aef9ba1bab9f18d7db1a6dce756
HuberM1998 <huber.mathaus@gmail.com>
2021-05-14 16:33:31 -0300

    Adicionando pagina de registro de usuarios

---
+++ src/views/Registro.vue
@@ -0,0 +1,45 @@
+<template>
+  <main>
+    <div class="container pt-6 pb-4">
+      <div class="columns">
+        <div class="column is-10 is-offset-1">
+          <div class="card ">
+            <div class="card-content">
+             <b-field>
+             <b-input placeholder="Nome"></b-input>
+             </b-field>
+
+              <b-field>
+              <b-input placeholder="Sobrenome"></b-input>
+              </b-field>
+              
+              <b-field>
+              <b-input placeholder="CPF"></b-input>
+              </b-field>
+
+              <b-input placeholder="Idade"
+                type="number"
+                min="18"
+                max="100">
+             </b-input>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Registro",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  background-color: $primary;
+  height: 100vh;
+}
+</style>
