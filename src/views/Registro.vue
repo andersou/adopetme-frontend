721eebd76240405ee8a817a9e420934e97a00c8f
HuberM1998 <huber.mathaus@gmail.com>
2021-05-14 18:06:54 -0300

    Atualizaçao pagina de registros

--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -5,23 +5,84 @@
         <div class="column is-10 is-offset-1">
           <div class="card ">
             <div class="card-content">
-             <b-field>
+              <b-field label="Foto Pessoal">
+            <b-upload v-model="dropFiles"
+                multiple
+                drag-drop>
+                <section class="section">
+                    <div class="content has-text-centered">
+                        <p>
+                            <b-icon
+                                icon="upload"
+                                size="is-large">
+                            </b-icon>
+                        </p>
+                        <p>Drop your files here or click to upload</p>
+                    </div>
+                </section>
+            </b-upload>
+        </b-field>
+
+        <div class="tags">
+            <span v-for="(file, index) in dropFiles"
+                :key="index"
+                class="tag is-primary" >
+                {{file.name}}
+                <button class="delete is-small"
+                    type="button"
+                    @click="deleteDropFile(index)">
+                </button>
+            </span>
+        </div>
+
+             <b-field label="Nome">
              <b-input placeholder="Nome"></b-input>
              </b-field>
 
-              <b-field>
+              <b-field label="Sobrenome">
               <b-input placeholder="Sobrenome"></b-input>
               </b-field>
               
-              <b-field>
+              <b-field label="CPF">
               <b-input placeholder="CPF"></b-input>
               </b-field>
 
+              <b-field grouped>
+              <b-field label="Gênero">
+                <b-select placeholder="Gênero">
+                <option>Masculino</option>
+                <option>Feminino</option>
+                <option>Não binário</option>
+                </b-select>
+              </b-field>
+              <b-field label="Idade">
               <b-input placeholder="Idade"
                 type="number"
                 min="18"
                 max="100">
-             </b-input>
+              </b-input>
+              </b-field>
+              </b-field>
+
+              <b-field grouped>
+              <b-field label="Senha" class="">
+                <b-input type="password"
+                value=""
+                password-reveal>
+                </b-input>
+              </b-field>
+
+              <b-field label="Confirmação de senha">
+                <b-input type="password"
+                value=""
+                password-reveal>
+                </b-input>
+              </b-field>
+              </b-field>
+
+              <div class="buttons">
+                <b-button type="is-primary" outlined>Enviar</b-button>
+              </div>
             </div>
           </div>
         </div>
@@ -34,6 +95,17 @@
 export default {
   name: "Registro",
   components: {},
+
+  data() {
+            return {
+                dropFiles: []
+            }
+        },
+        methods: {
+            deleteDropFile(index) {
+                this.dropFiles.splice(index, 1)
+            }
+        }
 };
 </script>
 
