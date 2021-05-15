9cfed2695ba33933eae572803b70b2ad3b41ecd3
Anderson Souza <andersom.eugenio@gmail.com>
2021-05-15 16:41:24 -0300

    adicionado material icons design font

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from "./store";
 
 // import "buefy/dist/buefy.css";
 import "./styles/style.scss";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Buefy);
 Vue.config.productionTip = false;
