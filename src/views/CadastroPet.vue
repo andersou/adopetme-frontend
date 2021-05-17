459ff1138ad62b61ad09a47f739670173c9fbc2d
HuberM1998 <huber.mathaus@gmail.com>
2021-05-17 00:45:22 -0300

    Modificaçao pagina de cadastro

--- src/views/CadastroPet.vue
+++ src/views/CadastroPet.vue
@@ -5,32 +5,38 @@
         <div class="column is-10 is-offset-1">
           <div class="card ">
             <div class="card-content">
-              <b-field label="Foto do Animal">
-                <b-upload v-model="dropFiles" multiple drag-drop>
-                  <section class="section">
-                    <div class="content has-text-centered">
-                      <p>
-                        <b-icon icon="upload" size="is-large"> </b-icon>
-                      </p>
-                      <p>Drop your files here or click to upload</p>
-                    </div>
-                  </section>
-                </b-upload>
-              </b-field>
-
-              <div class="tags">
-                <span
-                  v-for="(file, index) in dropFiles"
-                  :key="index"
-                  class="tag is-primary"
-                >
-                  {{ file.name }}
-                  <button
-                    class="delete is-small"
-                    type="button"
-                    @click="deleteDropFile(index)"
-                  ></button>
-                </span>
+               <div class="container">
+                <div class="is-flex is-flex-wrap-wrap is-justify-content-center mt-6 mb-6">
+                  <figure class="image ">
+                    <img
+                      class="img-avatar is-rounded"
+                      v-if="imageData"
+                      :src="imageData"
+                      alt=""
+                    />
+                    <img
+                      v-else
+                      class="img-avatar"
+                      src="../assets/pet.png"
+                    />
+                  </figure>
+                  <b-field label="Foto Pessoal" class=" has-text-centered">
+                    <b-upload
+                      v-model="avatar"
+                      accept="image/png,  image/jpeg"
+                      drag-drop
+                    >
+                      <section class="section">
+                        <div class="content has-text-centered">
+                          <p>
+                            <b-icon icon="upload" size="is-large"> </b-icon>
+                          </p>
+                          <p>Arraste a foto aqui</p>
+                        </div>
+                      </section>
+                    </b-upload>
+                  </b-field>
+                </div>
               </div>
 
               <b-field grouped>
@@ -57,9 +63,35 @@
                 </b-field>
               </b-field>
 
-              <b-field label="Peso">
-                <b-slider step="0.1" :value="20"> </b-slider>
-              </b-field>
+
+              <div class="columns">
+                <div class="column">
+                  <b-field label="Porte">
+            <b-radio-button v-model="radioButton"
+                native-value="Pequeno"
+                type="is-primary is-light is-outlined">
+                <span>Pequeno</span>
+            </b-radio-button>
+
+            <b-radio-button v-model="radioButton"
+                native-value="Médio"
+                type="is-primary is-light is-outlined">
+                <span>Médio</span>
+            </b-radio-button>
+
+            <b-radio-button v-model="radioButton"
+                native-value="Grande"
+                type="is-primary is-light is-outlined">
+                <span>Grande</span>
+            </b-radio-button>
+
+        </b-field>
+        <p class="content">
+            <b>Escolha:</b>
+            {{ radioButton }}
+        </p>
+                </div>
+              </div>
 
               <b-field label="Descrição" class="descricao">
                 <b-input maxlength="200" type="textarea"></b-input>
@@ -80,11 +112,14 @@
 export default {
   name: "Cadastro",
   components: {},
-
+  
   data() {
     const today = new Date();
 
     return {
+      avatar: null,
+      imageData: null,
+      radioButton: '',
       date: new Date(),
       minDate: new Date(
         today.getFullYear() - 80,
@@ -97,13 +132,32 @@ export default {
         today.getDate()
       ),
 
-      dropFiles: [],
     };
   },
-
   methods: {
-    deleteDropFile(index) {
-      this.dropFiles.splice(index, 1);
+    deleteDropFile() {
+      this.avatar = null;
+    },
+    previewImage: function() {
+      if (this.avatar) {
+        // create a new FileReader to read this image and convert to base64 format
+        var reader = new FileReader();
+        // Define a callback function to run, when FileReader finishes its job
+        reader.onload = (e) => {
+          // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
+          // Read image as base64 and set to imageData
+          this.imageData = e.target.result;
+        };
+        // Start the reader job - read file as a data url (base64 format)
+        reader.readAsDataURL(this.avatar);
+      }
+    },
+  },
+  watch: {
+    avatar(newAvatar) {
+      if (newAvatar) {
+        this.previewImage();
+      }
     },
   },
 };
@@ -111,9 +165,22 @@ export default {
 
 <style lang="scss" scoped>
 main {
-  background-color: $primary;
+  background:   url(../assets/capa.png),
+                url(../assets/ruido.png),
+                linear-gradient(50deg,#121758,#26a4f8);
+  background-attachment: fixed;
   //@include desktop {
   min-height: calc(100vh - 52px);
   //}
 }
+
+.img-avatar {
+  max-width: 280px;
+}
+@include tablet {
+  .img-avatar {
+    margin-right: 3rem;
+  }
+}
+
 </style>
