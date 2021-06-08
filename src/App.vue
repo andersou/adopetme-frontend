c6ce3b3c2ddbaee6219f6dbcfe58964e9c574ada
hpfred <frepantunes@yahoo.com.br>
2021-06-07 21:44:09 -0300

    Tanto faz

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <div id="app">
+      <a id="top"></a>
       <b-navbar class="navbar-adopetme">
         <template #brand>
           <b-navbar-item v-if="!user.id" tag="router-link" :to="{ path: '/' }">
@@ -129,10 +130,8 @@
                 >
                   Adotar um Pet
                 </a>
-              </p>
-              <p>
                 <a
-                  v-if="user.id"
+                  v-else
                   tag="router-link"
                   :to="{ path: '/adote-pet' }"
                 >
@@ -221,6 +220,7 @@
         <span class="logo-adopetme ">Adopetme</span>
       </footer>
       <!-- 
+      <img onclick="topFunction()" id="myBtn" src="../src/assets/AdoPetMe_Youtube.png" alt="" srcset=""/>
        -->
     </div>
   </main>
@@ -260,6 +260,17 @@ export default {
   font-size: 1.5rem;
 }
 .adopetme-social-media {
-  width: 800px;
+  width: 800px; //se largura eh 800px, pq ele vai mudando com redimensionamento?
 }
+// #myBtn {
+//   display: none; /* Hidden by default */
+//   position: fixed; /* Fixed/sticky position */
+//   bottom: 20px; /* Place the button at the bottom of the page */
+//   right: 30px; /* Place the button 30px from the right */
+//   z-index: 99; /* Make sure it does not overlap */
+//   border: none; /* Remove borders */
+//   outline: none; /* Remove outline */
+//   cursor: pointer; /* Add a mouse pointer on hover */
+//   padding: 15px; /* Some padding */
+// }
 </style>
