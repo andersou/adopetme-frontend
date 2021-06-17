8ad270f51416c5140831b52929891b4e689d35b1
Anderson Souza <andersom.eugenio@gmail.com>
2021-06-17 14:39:14 -0300

    adicionado ordenação na api do ibge

--- src/services/ibge.js
+++ src/services/ibge.js
@@ -4,10 +4,10 @@ const api = axios.create({
   baseURL: "https://servicodados.ibge.gov.br/api/v1",
 });
 function getMunicipios(uf) {
-  return api.get(`/localidades/estados/${uf}/municipios`);
+  return api.get(`/localidades/estados/${uf}/municipios?orderBy=nome`);
 }
 function getEstados() {
-  return api.get(`/localidades/estados`);
+  return api.get(`/localidades/estados?orderBy=nome`);
 }
 
 export { getMunicipios, getEstados };
